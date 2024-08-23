7c9b24ccaea69185cb4777d2f9f0473053b7fa18
tony-ngok <tonyn1999@outlook.com>
2024-08-23 11:48:52 -0500

    date, image, url

--- greenist/spiders/product.py
+++ greenist/spiders/product.py
@@ -1,10 +1,53 @@
+from datetime import datetime
+
 import scrapy
+from scrapy.http import Request, Response, HtmlResponse
 
 
+# python3 -m greenist_test.test_spiders.test_product
 class ProductSpider(scrapy.Spider):
     name = "greenist_spider"
     allowed_domains = ["greenist.de"]
     start_urls = ["https://greenist.de"]
 
-    def parse(self, response):
-        pass
+    def parse(self, response: HtmlResponse):
+        # breakpoint()
+
+        date = datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
+        url = response.css('meta[property="og:url"]::attr(content)').get().strip()
+        title = response.css('div.h1::text').get().strip()
+        images = response.css('div.product--image-container source[type="image/webp"]::attr(srcset)').get().strip()
+
+        yield {
+            "date": date,
+            "url": url,
+            "source": "Greenist",
+            "product_id": "",
+            "existence": None,
+            "title": title,
+            "title_en": None,
+            "description_en": None,
+            "summary": None,
+            "sku": None,
+            "upc": None,
+            "brand": "Janur",
+        	"specifications": None,
+            "categories": None,
+            "images": images,
+            "videos": None,
+            "price": None,
+            "available_qty": None,
+            "options": None,
+            "variants": None,
+            "returnable": None,
+            "reviews": None,
+            "rating": None,
+            "sold_count": None,
+            "shipping_fee": None,
+            "shipping_days_min": None,
+            "shipping_days_max": None,
+            "weight": None,
+            "width": None,
+            "height": None,
+            "length": None
+        }
