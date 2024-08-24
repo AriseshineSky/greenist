a4e589eb1c502a495ba1e649cfc60cacdf8b0a93
tony-ngok <tonyn1999@outlook.com>
2024-08-24 11:06:33 -0500

    vitalabo除变种、参数外解析完成

--- greenist/spiders/vitalabo.py
+++ greenist/spiders/vitalabo.py
@@ -1,6 +1,5 @@
 from datetime import datetime
 from json import loads
-from re import findall
 
 import scrapy
 from scrapy.http import Request, Response, HtmlResponse
@@ -34,28 +33,48 @@ class VitalaboSpider(scrapy.Spider):
         if 'instock' in prod_json['offers']['availability'].lower():
             existence = True
 
-        title = prod_json['name']
+        # title = prod_json['name']
+        title = ' '.join(response.css('h1.p-heading::text').getall()[1].strip().split())
 
-        # 待完成：description_en
+        # TODO: description_en
         descriptions = None
 
-        upc = prod_json['gtin13']
+        upc = prod_json['gtin13'][1:]
         brand = prod_json['brand']['name']
 
-        cat_sel = response.css('ul#js-breadcrumbs a')[1:]
+        cat_sel = response.css('ul#js-breadcrumbs a')[1:-1]
         categories = " > ".join([cat.css("::text").get().strip() for cat in cat_sel])
 
         img_sel = response.css('div.bigslider img.product__image')
-        images = ";".join([img.css("::src").get().strip().replace('256x256.jpg', '768x768.jpg') for img in img_sel])
-        price = round(float(prod_json['offers']['price'])*1.11, 2)
+        images = ";".join([img.css("::attr(src)").get().strip().replace('256x256.jpg', '768x768.jpg') for img in img_sel])
+
+        price_eu = float(prod_json['offers']['price'])
+        price = round(price_eu*1.11, 2)
+
+        available_qty = None
+        quant_sel = response.css('p.p-stock::attr(data-limit)')
+        if existence == False:
+            available_qty = 0
+        elif quant_sel:
+            available_qty = int(quant_sel.get().strip())
+
+        # TODO: ul.p-variants__list（点击并获取变种）
+
+
+        reviews = int(prod_json['aggregateRating']['ratingCount'])
+        rating = round(float(prod_json['aggregateRating']['ratingValue']), 1)
+
+        ppk = float(response.css('span.p-price__perunit::text').get().strip()[2:-5].replace(",", "."))
+        weight = round(price_eu/ppk*2.20462, 2)
 
         shipping_fee = round(prod_json['offers']['shippingDetails']['shippingRate']['value']*1.11, 2)
-        
+        shipping_days_min = prod_json['offers']['shippingDetails']['deliveryTime']['transitTime']['minValue']
+        shipping_days_max = prod_json['offers']['shippingDetails']['deliveryTime']['transitTime']['maxValue']
 
         yield {
             "date": date,
             "url": url,
-            "source": "Greenist",
+            "source": "Vitalabo",
             "product_id": product_id,
             "existence": existence,
             "title": title,
@@ -70,7 +89,7 @@ class VitalaboSpider(scrapy.Spider):
             "images": images,
             "videos": None,
             "price": price,
-            "available_qty": None,
+            "available_qty": available_qty,
             "options": None,
             "variants": None,
             "returnable": False,
--- vitalabo_test/test_spiders/test_product.py
+++ vitalabo_test/test_spiders/test_product.py
@@ -10,9 +10,136 @@ class TestProduct(unittest.TestCase):
         self.crawler = get_crawler(VitalaboSpider)
         self.spider = self.crawler._create_spider()
 
+    def test_variable_produkt(self):
+        url = "https://www.vitalabo.com/natures-plus/source-of-life-animal-parade"
+        body = None
+        with open(
+            "vitalabo_test/pages/source-of-life-animal-parade.htm",
+            "rb"
+        ) as file:
+            body = file.read()
+        response = HtmlResponse(
+            url=url,
+            body=body,
+        )
+        result = list(self.spider.parse(response))
+        self.assertEqual(len(result), 1)
+        product = result[0]
+        target_product = {
+            "product_id": "DS-29972",
+            "url": "https://www.vitalabo.com/natures-plus/source-of-life-animal-parade",
+            "source": "Vitalabo",
+            "existence": True,
+            "title": "Animal Parade 180 Chewable Multivitamins, Cherry (180 chewable tablets)",
+            "images": "https://va.nice-cdn.com/upload/image/product/large/default/75722_63cdfc9e.768x768.jpg;https://va.nice-cdn.com/upload/image/product/large/default/75723_0d774822.768x768.jpg",
+            "description_en": None,
+            "summary": None,
+            "upc": "097467299726",
+            "sku": "DS-29972",
+            "brand": "NaturesPlus",
+            "specifications": None,
+            "categories": 'Brands > NaturesPlus®',
+            "price": 47.16, # currency 1.11
+            "videos": None,
+            "shipping_fee": 0.00,
+            "available_qty": 2,
+            "reviews": 31,
+            "rating": 4.7,
+            "weight": 0.77, # kg -> lb 2.20462
+            "options": None,
+            "variants": None,
+        }
+        keys = [
+            "existence",
+            "product_id",
+            "sku",
+            "upc",
+            "url",
+            "source",
+            "brand",
+            "title",
+            "shipping_fee",
+            # "description_en",
+            # "specifications",
+            "images",
+            "price",
+            "categories",
+            "available_qty",
+            "reviews",
+            "rating",
+            "weight",
+            # "options",
+            # "variants",
+        ]
+        for key in keys:
+            self.assertEqual(product[key], target_product[key])
+
+
     def test_unavailable_product(self):
-        pass
-        # TODO
+        url = "https://www.vitalabo.com/natures-plus/animal-parade-inner-ear-support"
+        body = None
+        with open(
+            "vitalabo_test/pages/animal-parade-inner-ear-support.htm",
+            "rb",
+        ) as file:
+            body = file.read()
+        response = HtmlResponse(
+            url=url,
+            body=body,
+        )
+        result = list(self.spider.parse(response))
+        self.assertEqual(len(result), 1)
+        product = result[0]
+        target_product = {
+            "product_id": "DS-29949",
+            "url": "https://www.vitalabo.com/natures-plus/animal-parade-inner-ear-support",
+            "source": "Vitalabo",
+            "existence": False,
+            "title": "Animal Parade Inner Ear Support - Sugar-Free, 90 chewable tablets",
+            "images": "https://va.nice-cdn.com/upload/image/product/large/default/75587_57609dee.768x768.jpg;https://va.nice-cdn.com/upload/image/product/large/default/75588_26d494a9.768x768.jpg;https://va.nice-cdn.com/upload/image/product/large/default/75589_c81cbe37.768x768.jpg",
+            "description_en": None,
+            "summary": None,
+            "upc": "097467299498",
+            "sku": "DS-29949",
+            "brand": "NaturesPlus",
+            "specifications": None,
+            "categories": 'Brands > NaturesPlus®',
+            "price": 36.06, # currency 1.11
+            "videos": None,
+            "shipping_fee": 5.44,
+            "available_qty": 0,
+            "reviews": 3,
+            "rating": 5.0,
+            "weight": 0.42, # kg -> lb 2.20462 
+            "options": None,
+            "variants": None,
+        }
+
+        keys = [
+            "existence",
+            "product_id",
+            "sku",
+            "upc",
+            "url",
+            "source",
+            "brand",
+            "title",
+            "shipping_fee",
+            # "description_en",
+            # "specifications",
+            "images",
+            "price",
+            "categories",
+            "available_qty",
+            "reviews",
+            "rating",
+            "weight",
+            "options",
+            "variants",
+        ]
+        for key in keys:
+            self.assertEqual(product[key], target_product[key])
+
 
     def test_available_product_v1(self):
         url = "https://www.vitalabo.com/natures-plus/ultra-prenatal"
@@ -30,6 +157,7 @@ class TestProduct(unittest.TestCase):
         self.assertEqual(len(result), 1)
         product = result[0]
         target_product = {
+            "product_id": "DS-3085",
             "url": "https://www.vitalabo.com/natures-plus/ultra-prenatal",
             "source": "Vitalabo",
             "existence": True,
@@ -37,14 +165,20 @@ class TestProduct(unittest.TestCase):
             "images": "https://va.nice-cdn.com/upload/image/product/large/default/15257_33ceb058.768x768.jpg;https://va.nice-cdn.com/upload/image/product/large/default/76617_dbd755ec.768x768.jpg;https://va.nice-cdn.com/upload/image/product/large/default/76618_eaa8a183.768x768.jpg",
             "description_en": None,
             "summary": None,
-            "upc": None,
-            "sku": None,
+            "upc": "097467030855",
+            "sku": "DS-3085",
             "brand": "NaturesPlus",
             "specifications": None,
-            "categories": None,
+            "categories": 'Brands > NaturesPlus®',
             "price": 72.14, # currency 1.11
             "videos": None,
             "shipping_fee": 0.00,
+            "available_qty": 3,
+            "reviews": 12,
+            "rating": 5.0,
+            "weight": 1.03, # kg -> lb 2.20462
+            "options": None,
+            "variants": None,
         }
 
         keys = [
@@ -57,10 +191,83 @@ class TestProduct(unittest.TestCase):
             "brand",
             "title",
             "shipping_fee",
-            "specifications",
+            # "description_en",
+            # "specifications",
             "images",
             "price",
             "categories",
+            "available_qty",
+            "reviews",
+            "rating",
+            "weight",
+            "options",
+            "variants",
+        ]
+        for key in keys:
+            self.assertEqual(product[key], target_product[key])
+
+
+    def test_available_product_v2(self):
+        url = "https://www.vitalabo.com/natures-plus/ultra-bromelain"
+        body = None
+        with open(
+            "vitalabo_test/pages/ultra-bromelain.html",
+            "rb",
+        ) as file:
+            body = file.read()
+        response = HtmlResponse(
+            url=url,
+            body=body,
+        )
+        result = list(self.spider.parse(response))
+        self.assertEqual(len(result), 1)
+        product = result[0]
+        target_product = {
+            "product_id": "DS-4406",
+            "url": "https://www.vitalabo.com/natures-plus/ultra-bromelain",
+            "source": "Vitalabo",
+            "existence": True,
+            "title": "Ultra Bromelain, 60 tablets",
+            "images": "https://va.nice-cdn.com/upload/image/product/large/default/70020_6ddfe281.768x768.jpg;https://va.nice-cdn.com/upload/image/product/large/default/70021_1ca5f342.768x768.jpg",
+            "description_en": None,
+            "summary": None,
+            "upc": "097467044067",
+            "sku": "DS-4406",
+            "brand": "NaturesPlus",
+            "specifications": None,
+            "categories": 'Brands > NaturesPlus®',
+            "price": 61.04, # currency 1.11
+            "videos": None,
+            "shipping_fee": 0.00,
+            "available_qty": 4,
+            "reviews": 11,
+            "rating": 4.0,
+            "weight": 0.26, # kg -> lb 2.20462
+            "options": None,
+            "variants": None,
+        }
+
+        keys = [
+            "existence",
+            "product_id",
+            "sku",
+            "upc",
+            "url",
+            "source",
+            "brand",
+            "title",
+            "shipping_fee",
+            # "description_en",
+            # "specifications",
+            "images",
+            "price",
+            "categories",
+            "available_qty",
+            "reviews",
+            "rating",
+            "weight",
+            "options",
+            "variants",
         ]
         for key in keys:
             self.assertEqual(product[key], target_product[key])
