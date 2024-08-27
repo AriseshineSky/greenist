f1bd0a0b2ee18c20efa61acd9aad875160ec842a
tony-ngok <tonyn1999@outlook.com>
2024-08-27 09:32:12 -0500

    商品测试

--- dm_test/test_spiders/test_product.py
+++ dm_test/test_spiders/test_product.py
@@ -9,17 +9,72 @@ class TestProduct(unittest.TestCase):
         self.crawler = get_crawler(DmSpider)
         self.spider = self.crawler._create_spider()
 
-    def test_variable_produkt(self):
-        pass
-
-    def test_unavailable_product(self):
-        pass
-
-    def test_available_product_v1(self):
-        url = "https://www.dm.de/profissimo-hochglanz-oberflaechentuch-aus-mikrofaser-p4066447774344.html"
+    def test_unverfuegbar(self):
+        url = "https://products.dm.de/product/DE/products/detail/gtin/4008137009138"
         body = None
         with open(
-            "dm_test/pages/profissimo-hochglanz-oberflaechentuch-aus-mikrofaser-p4066447774344.html",
+            "dm_test/jsons/produkt_4008137009138.json",
+            "rb",
+        ) as file:
+            body = file.read()
+        response = HtmlResponse(
+            url=url,
+            body=body,
+        )
+        result = list(self.spider.parse(response))
+        self.assertEqual(len(result), 1)
+        product = result[0]
+        target_product = {
+            "url": "https://www.dm.de/bad-heilbrunner-fruechtetee-heisse-zitrone-mit-limette-15-beutel-p4008137009138.html",
+            "source": "dm",
+            "product_id": "1425051", # Artikelnummer意为商品号
+            "existence": True,
+            "title": "Früchtetee Heiße Zitrone mit Limette (15 Beutel), 37,5 g",
+            # "description": None,
+            "sku": "1425051",
+            "upc": "4008137009138",
+            "brand": "Bad Heilbrunner",
+            "categories": 'Gesundheit > Erkältung > Immunsystem stärken',
+            "images": "https://media.dm-static.com/images/f_auto,q_auto,c_fit,h_1200,w_1200/v1718668959/products/pim/4008137009138-3571267/bad-heilbrunner-fruechtetee-heisse-zitrone-mit-limette-15-beutel;https://media.dm-static.com/images/f_auto,q_auto,c_fit,h_1200,w_1200/v1719708700/products/pim/4008137009138-2820874/bad-heilbrunner-fruechtetee-heisse-zitrone-mit-limette-15-beutel;https://media.dm-static.com/images/f_auto,q_auto,c_fit,h_1200,w_1200/v1719708700/products/pim/4008137009138-2820873/bad-heilbrunner-fruechtetee-heisse-zitrone-mit-limette-15-beutel",
+            "videos": None,
+            "price": 3.16, # currency 1.11
+            "reviews": 53,
+            "rating": 4.7,
+            "shipping_fee": 5.52,
+            "shipping_days_min": 2,
+            "shipping_days_max": 3,
+            "weight": 0.08, # kg -> lb 2.204623
+        }
+
+        keys = [
+            "url",
+            "source",
+            "product_id",
+            "existence",
+            "title",
+            # "description",
+            "sku",
+            "upc",
+            "brand",
+            "categories",
+            "images",
+            "videos",
+            "price",
+            "reviews",
+            "rating",
+            "shipping_fee",
+            "shipping_days_min",
+            "shipping_days_max",
+            "weight",
+        ]
+        for key in keys:
+            self.assertEqual(product[key], target_product[key])
+
+    def test_verfuegbar_nr1(self):
+        url = "https://products.dm.de/product/DE/products/detail/gtin/4066447774344"
+        body = None
+        with open(
+            "dm_test/jsons/produkt_4066447774344.json",
             "rb",
         ) as file:
             body = file.read()
@@ -42,7 +97,7 @@ class TestProduct(unittest.TestCase):
             "brand": "Profissimo",
             "categories": 'Haushalt > Putzen & Reinigen > Putztücher, Schwämme & Co.',
             "images": "https://media.dm-static.com/images/f_auto,q_auto,c_fit,h_1200,w_1200/v1719794184/products/pim/4066447774344-1658996458/profissimo-hochglanz-oberflaechentuch-aus-mikrofaser;https://media.dm-static.com/images/f_auto,q_auto,c_fit,h_1200,w_1200/v1719794184/products/pim/4066447774344-248284225/profissimo-hochglanz-oberflaechentuch-aus-mikrofaser;https://media.dm-static.com/images/f_auto,q_auto,c_fit,h_1200,w_1200/v1719794184/products/pim/4066447774344-510520669/profissimo-hochglanz-oberflaechentuch-aus-mikrofaser",
-            "videos": "https://www.youtube.com/watch?v=-aDVJN4etow;https://www.youtube.com/watch?v=DqMl1hoRa4A;https://www.youtube.com/watch?v=JIfS7WP734g",
+            "videos": "https://www.youtube-nocookie.com/embed/-aDVJN4etow?rel=0&showinfo=0&modestbranding=1&playsinline=1;https://www.youtube-nocookie.com/embed/DqMl1hoRa4A?rel=0&showinfo=0&modestbranding=1&playsinline=1;https://www.youtube-nocookie.com/embed/JIfS7WP734g?rel=0&showinfo=0&modestbranding=1&playsinline=1",
             "price": 2.51, # currency 1.11
             "reviews": 1,
             "rating": 5.0,
@@ -70,6 +125,128 @@ class TestProduct(unittest.TestCase):
             "shipping_fee",
             "shipping_days_min",
             "shipping_days_max",
+        ]
+        for key in keys:
+            self.assertEqual(product[key], target_product[key])
+
+    def test_verfuegbar_nr2(self):
+        url = "https://products.dm.de/product/DE/products/detail/gtin/4066447731538"
+        body = None
+        with open(
+            "dm_test/jsons/produkt_4066447731538.json",
+            "rb",
+        ) as file:
+            body = file.read()
+        response = HtmlResponse(
+            url=url,
+            body=body,
+        )
+        result = list(self.spider.parse(response))
+        self.assertEqual(len(result), 1)
+        product = result[0]
+        target_product = {
+            "url": "https://www.dm.de/sportness-protein-wafer-bites-haselnuss-kakao-geschmack-p4066447731538.html",
+            "source": "dm",
+            "product_id": "1713565", # Artikelnummer意为商品号
+            "existence": True,
+            "title": "Protein Wafer Bites, Haselnuss-Kakao Geschmack, 30 g",
+            # "description": None,
+            "sku": "1713565",
+            "upc": "4066447731538",
+            "brand": "Sportness",
+            "categories": 'Ernährung > Sportnahrung > Sport Zubehör & Spezialprodukte',
+            "images": "https://media.dm-static.com/images/f_auto,q_auto,c_fit,h_1200,w_1200/v1724664465/products/pim/4066447731538-1879991598/sportness-protein-wafer-bites-haselnuss-kakao-geschmack;https://media.dm-static.com/images/f_auto,q_auto,c_fit,h_1200,w_1200/v1724664465/products/pim/4066447731538-773503654/sportness-protein-wafer-bites-haselnuss-kakao-geschmack;https://media.dm-static.com/images/f_auto,q_auto,c_fit,h_1200,w_1200/v1724664465/products/pim/4066447731538-1281065010/sportness-protein-wafer-bites-haselnuss-kakao-geschmack",
+            "videos": None,
+            "price": 1.05, # currency 1.11
+            "reviews": 30,
+            "rating": 4.5,
+            "shipping_fee": 5.52,
+            "shipping_days_min": 2,
+            "shipping_days_max": 3,
+            "weight": 0.07, # kg -> lb 2.204623
+        }
+
+        keys = [
+            "url",
+            "source",
+            "product_id",
+            "existence",
+            "title",
+            # "description",
+            "sku",
+            "upc",
+            "brand",
+            "categories",
+            "images",
+            "videos",
+            "price",
+            "reviews",
+            "rating",
+            "shipping_fee",
+            "shipping_days_min",
+            "shipping_days_max",
+            "weight",
+        ]
+        for key in keys:
+            self.assertEqual(product[key], target_product[key])
+
+    def test_verfuegbar_nr3(self):
+        url = "https://products.dm.de/product/DE/products/detail/gtin/8700216386760"
+        body = None
+        with open(
+            "dm_test/jsons/produkt_4066447731538.json",
+            "rb",
+        ) as file:
+            body = file.read()
+        response = HtmlResponse(
+            url=url,
+            body=body,
+        )
+        result = list(self.spider.parse(response))
+        self.assertEqual(len(result), 1)
+        product = result[0]
+        target_product = {
+            "url": "https://www.dm.de/oral-b-zahnseide-tiefenreinigung-gewachst-p8700216386760.html",
+            "source": "dm",
+            "product_id": "2477863", # Artikelnummer意为商品号
+            "existence": True,
+            "title": "Zahnseide Tiefenreinigung gewachst, 50 m",
+            # "description": None,
+            "sku": "2477863",
+            "upc": "8700216386760",
+            "brand": "Oral-B",
+            "categories": 'Gesundheit > Zahnpflege > Interdentalbürsten & Zahnseide',
+            "images": "https://media.dm-static.com/images/f_auto,q_auto,c_fit,h_1200,w_1200/v1723164578/products/pim/8700216386760-1126492259/oral-b-zahnseide-tiefenreinigung-gewachst;https://media.dm-static.com/images/f_auto,q_auto,c_fit,h_1200,w_1200/v1723682786/products/pim/8700216386760-347327848/oral-b-zahnseide-tiefenreinigung-gewachst;https://media.dm-static.com/images/f_auto,q_auto,c_fit,h_1200,w_1200/v1723682786/products/pim/8700216386760-828959191/oral-b-zahnseide-tiefenreinigung-gewachst;https://media.dm-static.com/images/f_auto,q_auto,c_fit,h_1200,w_1200/v1723682786/products/pim/8700216386760-1432081877/oral-b-zahnseide-tiefenreinigung-gewachst;https://media.dm-static.com/images/f_auto,q_auto,c_fit,h_1200,w_1200/v1723682786/products/pim/8700216386760-980437479/oral-b-zahnseide-tiefenreinigung-gewachst;https://media.dm-static.com/images/f_auto,q_auto,c_fit,h_1200,w_1200/v1723682786/products/pim/8700216386760-497056032/oral-b-zahnseide-tiefenreinigung-gewachst;https://media.dm-static.com/images/f_auto,q_auto,c_fit,h_1200,w_1200/v1723682786/products/pim/8700216386760-843492273/oral-b-zahnseide-tiefenreinigung-gewachst;https://media.dm-static.com/images/f_auto,q_auto,c_fit,h_1200,w_1200/v1723682786/products/pim/8700216386760-493508149/oral-b-zahnseide-tiefenreinigung-gewachst",
+            "videos": None,
+            "price": 4.38, # currency 1.11
+            "reviews": 0,
+            "rating": 0.0,
+            "shipping_fee": 5.52,
+            "shipping_days_min": 2,
+            "shipping_days_max": 3,
+            "length": 1968.50, # cm -> inch 0.393701
+        }
+
+        keys = [
+            "url",
+            "source",
+            "product_id",
+            "existence",
+            "title",
+            # "description",
+            "sku",
+            "upc",
+            "brand",
+            "categories",
+            "images",
+            "videos",
+            "price",
+            "reviews",
+            "rating",
+            "shipping_fee",
+            "shipping_days_min",
+            "shipping_days_max",
+            "length",
         ]
         for key in keys:
             self.assertEqual(product[key], target_product[key])
