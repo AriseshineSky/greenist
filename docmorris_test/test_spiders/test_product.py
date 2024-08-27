42132d1b6dbbd3e997ac4f672bf52bbf646d0d88
tony-ngok <tonyn1999@outlook.com>
2024-08-27 15:32:12 -0500

    更多测试：从标题解析商品尺寸

--- docmorris_test/test_spiders/test_product.py
+++ docmorris_test/test_spiders/test_product.py
@@ -9,70 +9,6 @@ class TestProduct(unittest.TestCase):
         self.crawler = get_crawler(DocMorrisSpider)
         self.spider = self.crawler._create_spider()
 
-    def test_unavailable_produkt(self):
-        # url = "https://products.dm.de/product/DE/products/detail/gtin/4008137009138"
-        # body = None
-        # with open(
-        #     "dm_test/jsons/produkt_4008137009138.json",
-        #     "rb",
-        # ) as file:
-        #     body = file.read()
-        # response = HtmlResponse(
-        #     url=url,
-        #     body=body,
-        # )
-        # result = list(self.spider.parse(response))
-        # self.assertEqual(len(result), 1)
-        # product = result[0]
-        # target_product = {
-        #     "url": "https://www.dm.de/bad-heilbrunner-fruechtetee-heisse-zitrone-mit-limette-15-beutel-p4008137009138.html",
-        #     "source": "dm",
-        #     "product_id": "1425051", # Artikelnummer意为商品号
-        #     "existence": False,
-        #     "title": "Früchtetee Heiße Zitrone mit Limette (15 Beutel), 37,5 g",
-        #     # "description": None,
-        #     "sku": "1425051",
-        #     "upc": "4008137009138",
-        #     "brand": "Bad Heilbrunner",
-        #     "categories": 'Gesundheit > Erkältung > Immunsystem stärken',
-        #     "images": "https://media.dm-static.com/images/f_auto,q_auto,c_fit,h_1200,w_1200/v1718668959/products/pim/4008137009138-3571267/bad-heilbrunner-fruechtetee-heisse-zitrone-mit-limette-15-beutel;https://media.dm-static.com/images/f_auto,q_auto,c_fit,h_1200,w_1200/v1719708700/products/pim/4008137009138-2820874/bad-heilbrunner-fruechtetee-heisse-zitrone-mit-limette-15-beutel;https://media.dm-static.com/images/f_auto,q_auto,c_fit,h_1200,w_1200/v1719708700/products/pim/4008137009138-2820873/bad-heilbrunner-fruechtetee-heisse-zitrone-mit-limette-15-beutel",
-        #     "videos": None,
-        #     "price": 3.16, # currency 1.11
-        #     "available_qty": 0,
-        #     "reviews": 53,
-        #     "rating": 4.7,
-        #     "shipping_fee": 5.49,
-        #     "shipping_days_min": 2,
-        #     "shipping_days_max": 3,
-        #     "weight": 0.08, # kg -> lb 2.204623
-        # }
-
-        # keys = [
-        #     "url",
-        #     "source",
-        #     "product_id",
-        #     "existence",
-        #     "title",
-        #     # "description",
-        #     "sku",
-        #     "upc",
-        #     "brand",
-        #     "categories",
-        #     "images",
-        #     "videos",
-        #     "price",
-        #     "available_qty",
-        #     "reviews",
-        #     "rating",
-        #     "shipping_fee",
-        #     "shipping_days_min",
-        #     "shipping_days_max",
-        #     "weight",
-        # ]
-        # for key in keys:
-        #     self.assertEqual(product[key], target_product[key])
-        pass
-
     def test_available_produkt_v1(self):
         url = "https://www.docmorris.de/docmorris-vitamin-d3-tabletten-800-ie/08068486"
         body = None
@@ -91,28 +27,28 @@ class TestProduct(unittest.TestCase):
         target_product = {
             "url": "https://www.docmorris.de/docmorris-vitamin-d3-tabletten-800-ie/08068486",
             "source": "DrMorris",
-            "product_id": "1713512", # Artikelnummer意为商品号
+            "product_id": "b807667a-b87d-40a7-ad50-0eb4aed4e0b1",
             "existence": True,
-            "title": "Hochglanz-Oberflächentuch aus Mikrofaser, 1 St",
+            "title": "DocMorris Vitamin D3 Tabletten 800 I.E. 60 St",
             # "description": None,
-            "sku": "1713512",
-            "upc": "4066447774344",
-            "brand": "Profissimo",
-            "categories": 'Haushalt > Putzen & Reinigen > Putztücher, Schwämme & Co.',
-            "images": "https://media.dm-static.com/images/f_auto,q_auto,c_fit,h_1200,w_1200/v1719794184/products/pim/4066447774344-1658996458/profissimo-hochglanz-oberflaechentuch-aus-mikrofaser;https://media.dm-static.com/images/f_auto,q_auto,c_fit,h_1200,w_1200/v1719794184/products/pim/4066447774344-248284225/profissimo-hochglanz-oberflaechentuch-aus-mikrofaser;https://media.dm-static.com/images/f_auto,q_auto,c_fit,h_1200,w_1200/v1719794184/products/pim/4066447774344-510520669/profissimo-hochglanz-oberflaechentuch-aus-mikrofaser",
-            "videos": "https://www.youtube-nocookie.com/embed/-aDVJN4etow?rel=0&showinfo=0&modestbranding=1&playsinline=1;https://www.youtube-nocookie.com/embed/DqMl1hoRa4A?rel=0&showinfo=0&modestbranding=1&playsinline=1;https://www.youtube-nocookie.com/embed/JIfS7WP734g?rel=0&showinfo=0&modestbranding=1&playsinline=1",
-            "price": 2.50, # currency 1.11
-            "reviews": 1,
-            "rating": 5.0,
-            "shipping_fee": 5.49,
-            "shipping_days_min": 2,
-            "shipping_days_max": 3,
+            "sku": "7YTK1Z5G", # SKU通常为8位
+            "upc": "08068486",
+            "brand": None,
+            "categories": 'Vitamine, Sport & Ernährung > Vitamine & Mineralstoffe > Vitamine > Vitamin D',
+            "images": "https://statics.docmorris.de/static/produkte/7YTK1Z5G/docmorris/ps420r/docmorris-vitamin-d3-tabletten-800-i-e-60-st-08068486-0-1714032136152762.jpeg;https://statics.docmorris.de/static/produkte/7YTK1Z5G/docmorris/ps420r/docmorris-vitamin-d3-tabletten-800-i-e-60-st-08068486-front-1714032135798240.jpeg;https://statics.docmorris.de/static/produkte/7YTK1Z5G/docmorris/ps420r/docmorris-vitamin-d3-tabletten-800-i-e-60-st-08068486-back-1714032135457247.jpeg;https://statics.docmorris.de/static/produkte/7YTK1Z5G/docmorris/ps420r/docmorris-vitamin-d3-tabletten-800-i-e-60-st-08068486-left-1714032135849092.jpeg;https://statics.docmorris.de/static/produkte/7YTK1Z5G/docmorris/ps420r/docmorris-vitamin-d3-tabletten-800-i-e-60-st-08068486-right-1714032136126479.jpeg",
+            "price": 3.32, # currency 1.11
+            "reviews": 0,
+            "rating": None,
+            "shipping_fee": 3.89,
+            "shipping_days_min": 0,
+            "shipping_days_max": 1,
         }
 
         keys = [
             "url",
             "source",
             "product_id",
+            "readable_id",
             "existence",
             "title",
             # "description",
@@ -150,23 +86,23 @@ class TestProduct(unittest.TestCase):
         target_product = {
             "url": "https://www.docmorris.de/kytta-schmerzsalbe/10832865",
             "source": "DocMorris",
-            "product_id": "1713565", # Artikelnummer意为商品号
+            "product_id": "722c92b9-f4ba-4ffb-8bad-efdda3d39171",
             "existence": True,
-            "title": "Protein Wafer Bites, Haselnuss-Kakao Geschmack, 30 g",
+            "title": "Kytta Schmerzsalbe 150 g",
             # "description": None,
-            "sku": "1713565",
-            "upc": "4066447731538",
-            "brand": "Sportness",
-            "categories": 'Ernährung > Sportnahrung > Sport Zubehör & Spezialprodukte',
-            "images": "https://media.dm-static.com/images/f_auto,q_auto,c_fit,h_1200,w_1200/v1724664465/products/pim/4066447731538-1879991598/sportness-protein-wafer-bites-haselnuss-kakao-geschmack;https://media.dm-static.com/images/f_auto,q_auto,c_fit,h_1200,w_1200/v1724664465/products/pim/4066447731538-773503654/sportness-protein-wafer-bites-haselnuss-kakao-geschmack;https://media.dm-static.com/images/f_auto,q_auto,c_fit,h_1200,w_1200/v1724664465/products/pim/4066447731538-1281065010/sportness-protein-wafer-bites-haselnuss-kakao-geschmack",
+            "sku": "FX5UP389",
+            "upc": "10832865",
+            "brand": "Kytta",
+            "categories": 'Arzneimittel & Gesundheit > Muskeln, Knochen, Gelenke > Stauchung, Prellung & Zerrung > Cremes & Gele',
+            "images": "https://statics.docmorris.de/static/produkte/FX5UP389/docmorris/ps420r/kytta-schmerzsalbe-150-g-10832865-0-1720620118087441.jpg;https://statics.docmorris.de/static/produkte/FX5UP389/docmorris/ps420r/kytta-schmerzsalbe-150-g-10832865-2-1720620118721051.jpeg;https://statics.docmorris.de/static/produkte/FX5UP389/docmorris/ps420r/kytta-schmerzsalbe-150-g-10832865-3-1720620118989838.jpeg;https://statics.docmorris.de/static/produkte/FX5UP389/docmorris/ps420r/kytta-schmerzsalbe-150-g-10832865-4-1720620119264784.jpeg;https://statics.docmorris.de/static/produkte/FX5UP389/docmorris/ps420r/kytta-schmerzsalbe-150-g-10832865-5-1720620119632811.jpeg;https://statics.docmorris.de/static/produkte/FX5UP389/docmorris/ps420r/kytta-schmerzsalbe-150-g-10832865-6-1720620119915471.jpeg;https://statics.docmorris.de/static/produkte/FX5UP389/docmorris/ps420r/kytta-schmerzsalbe-150-g-10832865-7-1720620120183847.jpeg",
             "videos": None,
-            "price": 1.05, # currency 1.11
-            "reviews": 30,
-            "rating": 4.5,
-            "shipping_fee": 5.49,
-            "shipping_days_min": 2,
-            "shipping_days_max": 3,
-            "weight": 0.07, # kg -> lb 2.204623
+            "price": 21.08, # currency 1.11
+            "reviews": 495,
+            "rating": 4.6,
+            "shipping_fee": 3.89,
+            "shipping_days_min": 0,
+            "shipping_days_max": 1,
+            "weight": 0.33, # kg -> lb 2.204623
         }
 
         keys = [
@@ -183,6 +119,195 @@ class TestProduct(unittest.TestCase):
             "images",
             "videos",
             "price",
+            "reviews",
+            "rating",
+            "shipping_fee",
+            "shipping_days_min",
+            "shipping_days_max",
+            "weight",
+        ]
+        for key in keys:
+            self.assertEqual(product[key], target_product[key])
+
+    def test_available_produkt_v3(self):
+        url = "https://www.docmorris.de/thermometer-10100-c-20-cm-lang/15656657"
+        body = None
+        with open(
+            "docmorris_test/htmls/thermometer-10100-c-20-cm-lang_15656657.html",
+            "rb",
+        ) as file:
+            body = file.read()
+        response = HtmlResponse(
+            url=url,
+            body=body,
+        )
+        result = list(self.spider.parse(response))
+        self.assertEqual(len(result), 1)
+        product = result[0]
+        target_product = {
+            "url": "https://www.docmorris.de/thermometer-10100-c-20-cm-lang/15656657",
+            "source": "DocMorris",
+            "product_id": "33286c28-f941-455c-8eae-b29bb4ddfe7e",
+            "existence": True,
+            "title": "Thermometer 10-100° C 20 cm lang 1 St",
+            # "description": None,
+            "sku": "FX5AG5Z3",
+            "upc": "15656657",
+            "brand": None,
+            "categories": 'Arzneimittel & Gesundheit > Haus- & Reiseapotheke > Hausapotheke > Erste Hilfe & Verbandmittel',
+            "images": "https://statics.docmorris.de/static/produkte/FX5AG5Z3/docmorris/ps420r/thermometer-10-1000-c-20-cm-lang-1-st-15656657-default-1714050672913044.jpeg",
+            "videos": None,
+            "price": 5.87, # currency 1.11
+            "reviews": 1,
+            "rating": 5.0,
+            "shipping_fee": 3.89,
+            "shipping_days_min": 0,
+            "shipping_days_max": 1,
+            "length": 7.87, # cm -> in 0.393701
+        }
+
+        keys = [
+            "url",
+            "source",
+            "product_id",
+            "existence",
+            "title",
+            # "description",
+            "sku",
+            "upc",
+            "brand",
+            "categories",
+            "images",
+            "videos",
+            "price",
+            "reviews",
+            "rating",
+            "shipping_fee",
+            "shipping_days_min",
+            "shipping_days_max",
+            "length",
+        ]
+        for key in keys:
+            self.assertEqual(product[key], target_product[key])
+
+    def test_available_produkt_v4(self):
+        url = "https://www.docmorris.de/eskompressen-steril-8f-10-x-20-cm/01407092"
+        body = None
+        with open(
+            "docmorris_test/htmls/eskompressen-steril-8f-10-x-20-cm_01407092.html",
+            "rb",
+        ) as file:
+            body = file.read()
+        response = HtmlResponse(
+            url=url,
+            body=body,
+        )
+        result = list(self.spider.parse(response))
+        self.assertEqual(len(result), 1)
+        product = result[0]
+        target_product = {
+            "url": "https://www.docmorris.de/eskompressen-steril-8f-10-x-20-cm/01407092",
+            "source": "DocMorris",
+            "product_id": "6e3b2311-0250-4002-b135-a2234b227ff6",
+            "existence": True,
+            "title": "ES-Kompressen steril 8f 10 x 20 cm 25X2 St",
+            # "description": None,
+            "sku": "LG1G161S",
+            "upc": "01407092",
+            "brand": None,
+            "categories": 'Arzneimittel & Gesundheit > Haus- & Reiseapotheke > Hausapotheke > Erste Hilfe & Verbandmittel',
+            "images": "https://statics.docmorris.de/static/produkte/FX5AG5Z3/docmorris/ps420r/thermometer-10-1000-c-20-cm-lang-1-st-15656657-default-1714050672913044.jpeg",
+            "videos": None,
+            "price": 28.29, # currency 1.11
+            "reviews": 1,
+            "rating": 5.0,
+            "shipping_fee": 0.00,
+            "shipping_days_min": 0,
+            "shipping_days_max": 1,
+            "weight": 0.44,
+            "width": 3.94, # cm -> in 0.393701
+            "length": 7.87
+        }
+
+        keys = [
+            "url",
+            "source",
+            "product_id",
+            "existence",
+            "title",
+            # "description",
+            "sku",
+            "upc",
+            "brand",
+            "categories",
+            "images",
+            "videos",
+            "price",
+            "reviews",
+            "rating",
+            "shipping_fee",
+            "shipping_days_min",
+            "shipping_days_max",
+            "weight",
+            "width",
+            "length"
+        ]
+        for key in keys:
+            self.assertEqual(product[key], target_product[key])
+
+    def test_unavailable_produkt(self):
+        url = "https://www.docmorris.de/hitzer-loesung/04982721"
+        body = None
+        with open(
+            "dm_test/htmls/hitzer-loesung_04982721.html",
+            "rb",
+        ) as file:
+            body = file.read()
+        response = HtmlResponse(
+            url=url,
+            body=body,
+        )
+        result = list(self.spider.parse(response))
+        self.assertEqual(len(result), 1)
+        product = result[0]
+        target_product = {
+            "url": "https://www.docmorris.de/hitzer-loesung/04982721",
+            "source": "DocMorris",
+            "product_id": "fe6912e5-4006-4e64-bc21-61f60569591e",
+            "existence": False,
+            "title": "Hitzer Lösung 30 ml",
+            # "description": None,
+            "sku": "FX5RAAYX",
+            "upc": "04982721",
+            "brand": None,
+            "categories": None,
+            "images": "https://statics.docmorris.de/static/docmorris/cms/ps420r/docmorris_fallback_6a098c8404.jpeg",
+            "videos": None,
+            "price": 33.68, # currency 1.11
+            "available_qty": 0,
+            "reviews": 0,
+            "rating": None,
+            "shipping_fee": 0.00,
+            "shipping_days_min": 0,
+            "shipping_days_max": 1,
+            "weight": 0.07
+        }
+
+        keys = [
+            "url",
+            "source",
+            "product_id",
+            "existence",
+            "title",
+            # "description",
+            "sku",
+            "upc",
+            "brand",
+            "categories",
+            "images",
+            "videos",
+            "price",
+            "available_qty",
             "reviews",
             "rating",
             "shipping_fee",
--- greenist/spiders/docmorris.py
+++ greenist/spiders/docmorris.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from json import loads
+from math import ceil
 
 import scrapy
 from scrapy.http import Request, Response, HtmlResponse
@@ -19,12 +20,15 @@ class DocMorrisSpider(scrapy.Spider):
             return
 
         url = 'https://www.docmorris.de'+prod_info['url']
-        product_id = prod_info['readableId']
+        product_id = prod_info['productId']
         existence = prod_info['available']
         title = prod_info['name']
 
         # TODO
         description = None
+
+        # https://squareup.com/us/en/the-bottom-line/operating-your-business/stock-keeping-unit#:~:text=SKU%20stands%20for%20%E2%80%9Cstock%20keeping,has%20a%20unique%20SKU%20number.
+        sku = prod_info['readableId'] # SKU通常为8位
 
         upc = prod_info['code']
         brand = prod_info['brand']
@@ -32,7 +36,25 @@ class DocMorrisSpider(scrapy.Spider):
 
         images = None
         if prod_info['images']:
-            images = ";".join([])
+            images = ";".join([img['variants']['420']['formats']['webp']['resolutions']['2x']['url'] for img in prod_info['images']])
+
+        price = round(prod_info['prices']['salesPrice']['value']*1.11, 2)
+
+        available_qty = None
+        if not existence:
+            available_qty = 0
+        
+        reviews = prod_info['reviewCount'] if prod_info['reviewCount'] else 0
+        rating = round(prod_info['rating'], 1) if (prod_info['rating'] is not None) else None
+        shipping_fee = 0.00 if price >= 21.09 else 3.89 # 19欧元及以上免运费
+
+        weight = None
+        width = None
+        height = None
+        length = None
+
+        menge, einheit = prod_info['packagingSize'].strip().lower().split()
+        if (einheit == 'ml') or (einheit == 'g'):
 
         yield {
             "date": datetime.now().strftime('%Y-%m-%dT%H:%M:%S'),
@@ -44,26 +66,26 @@ class DocMorrisSpider(scrapy.Spider):
             "title_en": None,
             "description": description,
             "summary": None,
-            "sku": product_id,
+            "sku": sku,
             "upc": upc,
             "brand": brand,
             "specifications": None,
             "categories": categories,
-            # "images": images,
-            # "videos": videos,
-            # "price": price,
-            # "available_qty": available_qty,
-            # "options": None,
-            # "variants": None,
+            "images": images,
+            "videos": None,
+            "price": price,
+            "available_qty": available_qty,
+            "options": None,
+            "variants": None,
             "returnable": False,
-            # "reviews": reviews,
-            # "rating": rating,
-            # "sold_count": None,
-            # "shipping_fee": 5.49,
-            # "shipping_days_min": 2,
-            # "shipping_days_max": 3,
-            # "weight": weight,
-            # "width": None,
-            # "height": None,
-            # "length": length
+            "reviews": reviews,
+            "rating": rating,
+            "sold_count": None,
+            "shipping_fee": shipping_fee,
+            "shipping_days_min": 0,
+            "shipping_days_max": 1,
+            "weight": weight,
+            "width": width,
+            "height": height,
+            "length": length
         }
